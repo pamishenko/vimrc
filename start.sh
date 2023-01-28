4699af2d4297565e126ef129425ee76f0afb6f19
Павел Мищенко <pavelmisenko@MacBook-Air-Pavel.local>
2023-01-29 02:50:41 +0300

    create init script

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+Pluginstall https://github.com/junegunn/vim-plug/:
+       curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cp .vimrc ~
+#!/bin/bash
+
+Pluginstall https://github.com/junegunn/vim-plug/:
+       curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cp .vimrc ~
+#!/bin/bash
+
+Pluginstall https://github.com/junegunn/vim-plug/:
+       curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cp .vimrc ~
+
